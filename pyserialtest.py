59a29c1868c6f2c12072c03a7b0c5153da5eec22
Elena Rangelov <elenarangelov04@gmail.com>
2022-04-20 15:29:47 -0400

    Update pyserialtest.py

--- pyserialtest.py
+++ pyserialtest.py
@@ -4,13 +4,13 @@ import time
 arduino = serial.Serial('COM3', 9600)
 time.sleep(2)
 
-print arduino.readline()
-print ("Enter '1' to turn 'on' the LED and '0' to turn LED 'off'")
+print(arduino.readline())
+print("Enter '1' to turn 'on' the LED and '0' to turn LED 'off'")
 
 while 1:
 
     var = raw_input()
-    print "You Entered :", var
+    print("You Entered :", var)
 
     if(var == '1'):
         arduino.write('1')
