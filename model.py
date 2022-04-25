35f42976fa30701234d91dab9a8821c515649d4f
RahelSelemon <67439760+RahelSelemon@users.noreply.github.com>
2022-04-25 15:35:40 -0400

    Update model.py

--- model.py
+++ model.py
@@ -20,29 +20,32 @@ print(outputs)
     
 
 
+<<<<<<< Updated upstream
 
 X, y = df.values[:, :-1], df.values[:, -1]
+=======
+>>>>>>> Stashed changes
 # ensure all data are floating point values
-X = X.astype('float32')
+inputs = inputs.astype('float32')
 # encode strings to integer
-y = LabelEncoder().fit_transform(y)
 
-print(X_train.shape, X_test.shape)
+print(inputs.shape)
 # determine the number of input features
-n_features = X_train.shape[1]
+n_features = inputs.shape[1]
 
 model = Sequential()
 model.add(Dense(10, activation='relu', kernel_initializer='he_normal', input_shape=(n_features,)))
 model.add(Dense(8, activation='relu', kernel_initializer='he_normal'))
 model.add(Dense(26, activation='softmax'))
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-model.fit(X_train, y_train, epochs=150, batch_size=32, verbose=0)
+model.fit(inputs, outputs, epochs=150, batch_size=32, verbose=0)
 
 
-# evaluate the model
-loss, acc = model.evaluate(X_test, y_test, verbose=0)
-print('Test Accuracy: %.3f' % acc)
 # make a prediction
 row = [5.1,3.5,1.4,0.2]
 yhat = model.predict([row])
+<<<<<<< Updated upstream
 print('Predicted: %s (class=%d)' % (yhat, argmax(yhat)))
+=======
+#print('Predicted: %s (class=%d)' % (yhat, argmax(yhat)))
+>>>>>>> Stashed changes
